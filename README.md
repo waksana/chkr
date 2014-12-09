18337e37de668fc6fd4a6a9957eaf9580428aab0
waksana <waksana@qq.com>
2014-12-09 19:06:25 +0800

    every thing is done

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ kao
 ===
 
 Koa is Awesome Oh yeah
+
+```javascript
+var Kao = require('kao');
+var k = new Kao();
+k.check('field', function() {
+  if(!('field' in this))
+    throw new Error('i need this field');
+  if('string' == this.field) {
+    return this.field;
+  }
+  else if(this.field.toString) {
+    return this.field.toString();
+  }
+  throw new Error('i need this field with type string');
+});
+
+// you can pass arguments with body query or url
+k.route('get /hello/:field', function *(field, field2, whatever) {
+  this.body = 'hehe';
+});
+
+app.use(k.middleware());
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+var Router = require('koa-router');
+var util = require('util');
+
+var Kao = module.exports = function() {
+  this._fields = {};
+  this.router = new Router();
+};
+
+Kao.prototype.check = function(field, checker) {
+  this._fields[field] = checker;
+};
+
+Kao.prototype.route = function(cmd, fn) {
+  cmd = cmd.split(' ');
+  var args = annotate(fn);
+  var fields = this._fields;
+  this.router[cmd[0]](cmd[1], function *() {
+    var data = {};
+    util._extend(data, this.request.body);
+    util._extend(data, this.query);
+    util._extend(data, this.params);
+    data._userId = this.token;
+    data.method = this.method;
+    try{
+      var params = args.map(checker(data, fields));
+      this.body = yield fn.apply(this, params);
+    }
+    catch(e) {
+      this.body = e.message;
+      console.log(e.stack);
+    }
+  });
+};
+
+Kao.prototype.middleware = function() {
+  return this.router.middleware();
+};
+
+function checker(data, fields) {
+  return function(field) {
+    var value = data[field];
+    if(fields[field])
+      return fields[field].call(data);
+    if(value != undefined && value != null)
+      return value;
+    throw new Error(field + ' is required');
+  };
+}
+
+function annotate(fn) {
+  var part = fn.toString().split(')')[0];
+  return part
+  .substr(part.indexOf('(') + 1)
+  .split(',')
+  .map(function(p) {return p.trim();});
+}
